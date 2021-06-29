2c412f8ea2c35ae6a7217e0af9008baeb390da17
Kralizets <Kralizets@outlook.com>
2021-06-29 03:23:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ForExample29062021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = new List<int> { 1, 2, 3, 4, 5 };
+            List<string> secondList = new List<string> { "1", "2", "3", "4" };
+            List<double> thirdList = new List<double>() { 0.5, 0.6, 0.7 };
+            
+            ShowArray<int> showFirstArray = new ShowArray<int>(firstList);
+            ShowArray<string> showSecondArray = new ShowArray<string>(secondList);
+            ShowArray<double> showThirdArray = new ShowArray<double>(thirdList);
+
+            showFirstArray.Show();
+            showSecondArray.Show();
+            showThirdArray.Show();
+        }
+    }
+}
---
+++ ShowArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForExample29062021
+{
+    public class ShowArray<T>
+    {
+        private T[] _array;
+
+        public ShowArray(List<T> inputList)
+        {
+            _array = (T[])inputList.ToArray().Clone();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine("item [" + i + "] = " + _array[i]);
+            }
+        }
+    }
+}
